068d75dfc0099e036c71643ec40ad569273b0127
Maksim <70702008m@gmail.com>
2026-01-12 12:48:34 +0200

    teter

--- ryt.js
+++ ryt.js
@@ -1,8 +1,8 @@
 // console.log("JavaScript is awesome!");
 // console.log(10);
 
-const age = 20;
-let username = "Macsim";
-username = "ivan";
-console.log(age);
-console.log(username);
+// const age = 20;
+// let username = "Macsim";
+// username = "ivan";
+// console.log(age);
+// console.log(username);
---
+++ tytyty.js
@@ -0,0 +1,32 @@
+// function findNumber(max, target) {
+// 		for (let i = 1; i <= max; i += 1) {
+// 	    		    if (i === target) {
+// 	     				return i;
+// 	    }
+// 	  }
+//   }
+
+// const result = findNumber(10, 6);
+// console.log(`Result of function execution ${result}`);
+                                                        
+
+
+
+
+                                                                    //    \\\\\\\\\
+                                               // первое правдивое !!!!!
+
+console .log(3 || 7);
+console .log(`` || 7);
+ console .log(3 || 0);                     
+console .log(3 || 7 || 0);
+// console .log(NaN || 0 || undefined); undefined
+
+
+// console .log(3 && 7);
+// console .log(`` && 7);
+// console .log(3 && 0);             последнее правдивое или если не правдивое виводит неправдивое.
+// console .log(3 && 7 && 0);
+
+                                                              //    \\\\\\\\\\\\
+
