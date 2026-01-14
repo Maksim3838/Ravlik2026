cd7521d71cedef4ade892bc822b93ee81f99bc67
Maksim <70702008m@gmail.com>
2026-01-14 21:50:38 +0200

    text

---
+++ 3модуль.js
@@ -0,0 +1,50 @@
+// function getLastElementMeta(array) {
+//     const ryt = array.length - 1;
+    
+//   const ryj = array[ryt]
+// return[ryt, ryj]
+// }
+// getLastElementMeta(["apple", "peach", "pear", "banana"])
+
+// const a = [5];
+// const b = a;
+// console.log(a);
+// console.log(b);
+
+// a[0]= 10;
+// console.log(a);
+// console.log(b);
+
+
+// function calculateEngravingPrice(message, pricePerWord) {
+//     const words = message.split(" ");
+//     console.log(words.length*pricePerWord);
+// }
+
+
+// calculateEngravingPrice("JavaScript is in my blood", 10); // 50
+// calculateEngravingPrice("JavaScript is in my blood", 20); // 100
+// calculateEngravingPrice("Web-development is creative work", 40); // 160
+// calculateEngravingPrice("Web-development is creative work", 20); // 80
+
+// const planets = ["Earth", "Mars", "Venus"];
+// for (let i = 0; i < planets.length; i += 1){
+//     console.log(planets[i])
+// }
+
+
+// const tytyty = [1, 2, 3, 4, 5, 6];
+// const lalala = [1, 5];
+
+// function trrys(tytyty, lalala) {
+//     const zbn = [];
+//     for (const element of tytyty) { 
+//         if (lalala.includes(element)) {
+//             zbn.push(element);
+//         }
+//         console.log(zbn);
+//     };
+//     return zbn;
+// }
+
+// trrys(tytyty, lalala);
