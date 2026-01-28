62020242696328a431445063c739b6e402ba3980
Maksim <70702008m@gmail.com>
2026-01-28 20:47:24 +0200

    test

--- 5module.js
+++ 5module.js
@@ -89,24 +89,165 @@
 //     })
 
 
-const tututu = [
-    { mace: `Kia`, model: `sporteg`, price: 20000 },
-    { mace: `Lexys`, model: `top`, price: 80000 },
-    { mace: `Deo`, model: `lanos`, price: 1000 },
-];
+// const tututu = [
+//     { mace: `Kia`, model: `sporteg`, price: 20000 },
+//     { mace: `Lexys`, model: `top`, price: 80000 },
+//     { mace: `Deo`, model: `lanos`, price: 1000 },
+// ];
 
 
-const retCars = (car,discont) => {
-    return car.map(cars => {
-        return {
-            ...cars,
-            price: cars.price * (1 - discont)
-        }
-    }
-    );
+// const retCars = (car,discont) => {
+//     return car.map(cars => {
+//         return {
+//             ...cars,
+//             price: cars.price * (1 - discont)
+//         }
+//     }
+//     );
     
- };
+//  };
 
 
 
-console.table(retCars(tututu, 0.5));
+// console.table(retCars(tututu, 0.5));
+
+
+// const getUserNames = (users) => {
+//  return users.map((item) => {return item.name})
+// }
+
+
+
+// console.log(
+//   getUserNames([
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     balance: 2811
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     balance: 3821
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     balance: 3793
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     balance: 2278
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     balance: 3951
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     balance: 1498
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     balance: 2764
+//   },
+// ])
+// ); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+
+
+//
+
+
+
+
+
+const sortByDescendingFriendCount = (users) => { 
+return users.toSorted((a, b) => b.friends.length - a.friends.length)
+};
+
+
+
+
+
+console.table(
+  sortByDescendingFriendCount([
+    {
+      name: "Moore Hensley",
+      friends: ["Sharron Pace"],
+      gender: "male"
+    },
+    {
+      name: "Sharlene Bush",
+      friends: ["Briana Decker", "Sharron Pace"],
+      gender: "female"
+    },
+    {
+      name: "Ross Vazquez",
+      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+      gender: "male"
+    },
+    {
+      name: "Elma Head",
+      friends: ["Goldie Gentry", "Aisha Tran"],
+      gender: "female"
+    },
+    {
+      name: "Carey Barr",
+      friends: ["Jordan Sampson", "Eddie Strong"],
+      gender: "male"
+    },
+    {
+      name: "Blackburn Dotson",
+      friends: ["Jacklyn Lucas", "Linda Chapman"],
+      gender: "male"
+    },
+    {
+      name: "Sheree Anthony",
+      friends: ["Goldie Gentry", "Briana Decker"],
+      gender: "female"
+    }
+  ])
+);
+// [
+//   {
+//     name: "Ross Vazquez",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Carey Barr",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Moore Hensley",
+//     friends: ["Sharron Pace"],
+//     gender: "male"
+//   }
+// ]
+
+
